f2d646d14a33b322e1bcccfb3111ab6eaced49e0
unknown <858909828@qq.com>
2020-02-13 17:13:17 +0800

    add index

--- src/main/java/life/majiang/community/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -2,14 +2,14 @@ package life.majiang.community.community.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloController {
-    @RequestMapping("/hello")
-    @ResponseBody
-    public  String  hello(){
-        return  "hello";
+public class IndexController {
+   @GetMapping("/")
+    public  String  index(){
+        return  "index";
     }
 }
