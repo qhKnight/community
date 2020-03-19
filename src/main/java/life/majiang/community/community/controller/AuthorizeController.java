7ce5118328dcce0c61282375a78fa31ad3081f10
unknown <858909828@qq.com>
2020-03-19 16:08:41 +0800

    2020/3/19

--- src/main/java/life/majiang/community/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/community/controller/AuthorizeController.java
@@ -4,17 +4,28 @@ import life.majiang.community.community.dto.AccessTokenDto;
 import life.majiang.community.community.dto.GithubUser;
 import life.majiang.community.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.SQLOutput;
 
 /**
  * Created by qhKnight on 2020/2/16
  */
 @Controller
 public class AuthorizeController {
+
     @Autowired
-    GithubProvider githubProvider;
+    private GithubProvider githubProvider;
+
+    @Value("${github.Client.id}")
+    private String ClientId;
+    @Value("${github.Client.secret}")
+    private String ClientSecret;
+    @Value("${github.Redirect.uri}")
+    private String RedirectUri;
 
     @GetMapping("/callback")
     public String index(@RequestParam(name = "code") String code,
@@ -22,12 +33,13 @@ public class AuthorizeController {
         AccessTokenDto accessTokenDto = new AccessTokenDto();
         accessTokenDto.setCode(code);
         accessTokenDto.setState(state);
-        accessTokenDto.setClient_id("8085b33655ffaba0c283");
-        accessTokenDto.setClient_secret("a23ebd33bb80676b1ce1134d1d21c5ec5428b8a4");
-        accessTokenDto.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDto.setClient_id(ClientId);
+        accessTokenDto.setClient_secret(ClientSecret);
+        accessTokenDto.setRedirect_uri(RedirectUri);
         String accessToken = githubProvider.getAccessTokenDto(accessTokenDto);
         GithubUser githubUser = githubProvider.getUser(accessToken);
         System.out.println(githubUser.getName());
+        //System.out.println(ClientId);
         return "index";
     }
 }
