56e2dec85e9419b6d8576634b2dd39dfe3332a0b
unknown <858909828@qq.com>
2020-03-27 23:39:41 +0800

    web

--- src/main/java/life/majiang/community/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -6,12 +6,12 @@ import life.majiang.community.community.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 
 /**
  * Created by qhKnight on 2020/2/16
--- src/main/java/life/majiang/community/community/controller/PublishController.java
+++ src/main/java/life/majiang/community/community/controller/PublishController.java
@@ -4,6 +4,9 @@ package life.majiang.community.community.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Created by qhKnight on 2020/3/20
+ */
 @Controller
 public class PublishController {
     @GetMapping("publish")
